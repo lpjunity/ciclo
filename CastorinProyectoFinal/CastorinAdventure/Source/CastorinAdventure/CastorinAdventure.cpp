8fe225b3057a0be8e75a6fc2b3233e9a56ccd929
lpjdev <lpjunity25@gmail.com>
2024-05-13 12:55:58 +0100

    Removed previous C++ project

--- CastorinProyectoFinal/CastorinAdventure/Source/CastorinAdventure/CastorinAdventure.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CastorinAdventure.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CastorinAdventure, "CastorinAdventure" );
- 
