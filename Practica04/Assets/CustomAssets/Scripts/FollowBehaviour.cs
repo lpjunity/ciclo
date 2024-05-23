31011a50867379e6302c4f5dd1cf335260fe664e
Laura <lpjunity25@gmail.com>
2024-05-23 19:46:19 +0100

    Remove upper part logic, more lvl logic with toolbox and removing collision on cinematics

--- Practica04/Assets/CustomAssets/Scripts/FollowBehaviour.cs
+++ Practica04/Assets/CustomAssets/Scripts/FollowBehaviour.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class FollowBehaviour : MonoBehaviour
 {
     private GameObject _player;
+    private Collider[] _colliders;
     [SerializeField] private float _movementSpeed;
 
     // Start is called before the first frame update
@@ -16,8 +17,24 @@ public class FollowBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(_player.transform.position);
-        transform.position = Vector3.MoveTowards(transform.position, _player.transform.position, _movementSpeed / 100);
+
+        //transform.LookAt(_player.transform.position);
+        //transform.position = Vector3.MoveTowards(transform.position, _player.transform.position, _movementSpeed / 100);
+
+        //Fire a ray
+        //Get position from ray
+
+        LayerMask layerMask = LayerMask.GetMask("Player");
+        _colliders = Physics.OverlapSphere(transform.position, 20f, layerMask);
+        
+        foreach(var collider in _colliders)
+        {
+            if (collider.CompareTag("Player"))
+            {
+                transform.LookAt(_player.transform.position);
+                transform.position = Vector3.MoveTowards(transform.position, _player.transform.position, _movementSpeed / 100);
+            }
+        }
 
     }
 }
